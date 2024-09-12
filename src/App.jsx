0e4a34373e548dcb7541cce8a433f28fa5c95f9a
Hicham Abderrahim <hichamdevpro@gmail.com>
2024-09-12 13:18:11 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="grid min-h-screen place-content-center bg-blue-300">
+      <h1 className="text-4xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
